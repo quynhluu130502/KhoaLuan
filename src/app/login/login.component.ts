d34291c5e58d7fcf21f313e95c81345aa337df28
quynhluu130502 <quynhluu1305@gmail.com>
2024-01-14 20:18:38 +0700

    Cấu hình để có thể dùng được alertifyjs

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Route, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
+import * as alertify from 'alertifyjs';
 
 @Component({
   selector: 'app-login',
@@ -13,14 +14,21 @@ export class LoginComponent implements OnInit {
   constructor(private _router: Router, private _auth: AuthService) {}
   ngOnInit() {
     this.loginForm = new FormGroup({
-      username: new FormControl('', Validators.required),
+      username: new FormControl('', [Validators.required, Validators.maxLength(10)]),
       password: new FormControl('', Validators.required),
     });
+  }
+  get username(): any {
+    return this.loginForm.get('username');
+  }
+  get password(): any {
+    return this.loginForm.get('password');
   }
   onFormSubmit(): void {
     const usernameControl = this.loginForm.get('username');
     const passwordControl = this.loginForm.get('password');
     if (this._auth.login(this.loginForm.value) === true) {
+      alertify.success('message');
       this._router.navigate(['/dashboard']);
     }
   }
